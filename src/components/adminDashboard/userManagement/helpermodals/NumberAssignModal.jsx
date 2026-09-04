fe0e9c11ff689c65e54f5f02bf6015ae13296221
saim625 <saimsaeed625@gmail.com>
2026-09-04 23:42:12 +0500

    aligned field for name n number

--- src/components/adminDashboard/userManagement/helpermodals/NumberAssignModal.jsx
+++ src/components/adminDashboard/userManagement/helpermodals/NumberAssignModal.jsx
@@ -88,7 +88,7 @@ const NumberAssignModal = ({
           </h3>
 
           <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+            <div className="grid grid-cols-2 gap-4">
               <div>
                 <label className="block text-sm font-medium text-gray-700 mb-2">
                   Name
